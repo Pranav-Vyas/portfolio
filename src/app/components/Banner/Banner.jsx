bc045c0024f172d2208669e2938018441f38686b
pranav.vyas <pranav.vyas@pudle.in>
2023-07-09 23:29:49 +0530

    added clip path on banner

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -16,6 +16,7 @@ const cedarvilleCursive = Cedarville_Cursive({
 function Banner() {
   return (
     <section className={styles.wrapper}>
+      <div className={styles.background}></div>
       <header className={styles.header}>
         <h3 className={styles.name}>Pranav Vyas</h3>
         <nav className={styles.navigation}>
@@ -57,7 +58,16 @@ function Banner() {
         </nav>
       </header>
       <div className={styles.centerTextBox}>
-        <h1 className={styles.headingPrimaryWhite}>Hi, I&apos;m Pranav</h1>
+        {/* <div className={styles.greeting}>
+          <ul className={styles.greeting__list}>
+            <li className={styles.greeting__list__item}>Hello</li>
+            <li className={styles.greeting__list__item}>नमस्कार</li>
+            <li className={styles.greeting__list__item}>ನಮಸ್ಕಾರ</li>
+            <li className={styles.greeting__list__item}>হ্যালো</li>
+            <li className={styles.greeting__list__item}>こんにちは</li>
+          </ul>
+        </div> */}
+          <h1 className={styles.headingPrimaryWhite}>Hello, I&apos;m Pranav</h1>
         <h1 className={styles.headingPrimaryYellow}>A Software Engineer</h1>
       </div>
     </section>
