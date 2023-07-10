dec3d9ee15c79ef09ec27b04b4081313846d3399
pranav.vyas <pranav.vyas@pudle.in>
2023-07-10 13:46:27 +0530

    changed about and added resume button

--- src/app/components/Overview/Overview.jsx
+++ src/app/components/Overview/Overview.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 import styles from "./overview.module.scss";
+import Link from "next/link";
 
 function Overview() {
   return (
     <section className={styles.wrapper}>
       <div className={styles.textWrapper}>
         <h1 className={styles.headingPrimaryWhite}>About Me</h1>
-        <h1 className={styles.headingPrimaryYellow}>I am Pranav Vyas from India. I am a software engineer with specialization in web development.</h1>
+        <h1 className={styles.headingPrimaryYellow}>
+          I am Pranav Vyas, a software engineer with specialization in web
+          development, having a knack of writing elegant code and developing
+          applications using cutting-edge technologies.
+        </h1>
+        <Link className={styles.button} href="https://drive.google.com/file/d/15k7Iz3dIVLlr7x6gX0b3X83LDCSzEpia/view?usp=sharing">Resume</Link>
       </div>
       <div className={styles.imageWrapper}>
         <figure className={styles.profileShape}>
-        <img className={styles.profileImage} src="/images/Profile-img-transparent.png" alt="Profile Pic" />
+          <img
+            className={styles.profileImage}
+            src="/images/Profile-img-transparent.png"
+            alt="Profile Pic"
+          />
         </figure>
       </div>
     </section>
-  )
+  );
 }
 
-export default Overview
+export default Overview;
