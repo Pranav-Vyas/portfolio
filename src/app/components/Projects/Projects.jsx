ca5f53998a983436e410360e66156b15d1c4d31a
pranav.vyas <pranav.vyas@pudle.in>
2023-07-12 11:02:25 +0530

    added favicon and max width to sections

--- src/app/components/Projects/Projects.jsx
+++ src/app/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 import styles from "./projects.module.scss";
-import ProjectCard from '../Cards/ProjectCard/ProjectCard';
+import ProjectCard from "../Cards/ProjectCard/ProjectCard";
 import TwitterImage from "../../../../public/images/twitter-cover.jpg";
 import BloodDonationImage from "../../../../public/images/blood-donation.jpg";
 import PortfolioImage from "../../../../public/images/portfolio-cover.jpg";
@@ -10,12 +10,30 @@ function Projects() {
     <section className={styles.wrapper}>
       <h1 className={styles.heading}>Projects</h1>
       <div className={styles.grid}>
-      <ProjectCard image={TwitterImage} alt={"Twitter Image"} text={"Twitter-Clone"} subText={"MERN stack"} url={"https://github.com/Pranav-Vyas/Twitter-Clone"}/>
-      <ProjectCard image={BloodDonationImage} alt={"Blood Donation Image"} text={"Redbank"} subText={"HTML, CSS, Nodejs, MongoDB"} url={"https://github.com/Pranav-Vyas/RedBank"}/>
-      <ProjectCard image={PortfolioImage} alt={"Portfolio Image"} text={"Portfolio"} subText={"Next.js"} url={"https://github.com/pranav-Vyas/portfolio"}/>
+        <ProjectCard
+          image={TwitterImage}
+          alt={"Twitter Image"}
+          text={"Twitter-Clone"}
+          subText={"MERN stack"}
+          url={"https://github.com/Pranav-Vyas/Twitter-Clone"}
+        />
+        <ProjectCard
+          image={BloodDonationImage}
+          alt={"Blood Donation Image"}
+          text={"Redbank"}
+          subText={"HTML, CSS, Nodejs, MongoDB"}
+          url={"https://github.com/Pranav-Vyas/RedBank"}
+        />
+        <ProjectCard
+          image={PortfolioImage}
+          alt={"Portfolio Image"}
+          text={"Portfolio"}
+          subText={"Next.js"}
+          url={"https://github.com/pranav-Vyas/portfolio"}
+        />
       </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
