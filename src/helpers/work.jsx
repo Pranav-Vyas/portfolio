d50e500f3ae6c73db4da47555e53886043645385
Pranav Vyas <pranavvyas2016@gmail.com>
2024-01-14 11:36:20 +0530

    added accenture in work

--- src/helpers/work.jsx
+++ src/helpers/work.jsx
@@ -1,9 +1,18 @@
 export const work = [
   {
-    id: "1",
+    id: "3",
+    designation: "Associate Software Engineer",
+    year: "September 2023 - Present",
+    organization: "Accenture",
+    content: [
+      "Completed training on Cloud Native Microservices",
+    ]
+  },
+  {
+    id: "2",
     designation: "Associate Software Engineer Intern",
     year: "January 2023 - June 2023",
-    organization: "Turnkey Tech Solutions",
+    organization: "Onelab Ventures",
     content: [
       "Worked on developing MVP from scratch of a project based on Reactjs, Nodejs, Expresjs, PostgreSQL.",
       "Optimized and Reduced code duplicacy from 27% to 13%.",
@@ -12,7 +21,7 @@ export const work = [
     ]
   },
   {
-    id: "2",
+    id: "1",
     designation: "Software Engineer Intern",
     year: "April 2022 - July 2023",
     organization: "Puddle",
